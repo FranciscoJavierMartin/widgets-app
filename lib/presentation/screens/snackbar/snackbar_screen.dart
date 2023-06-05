f166c800642adf8789e554987b6c2086950cc263
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-06-05 18:30:08 +0200

    Snackbar

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,12 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   static const String name = 'snackbar_screen';
 
   const SnackbarScreen({super.key});
 
+  void showCustomSnackbar(BuildContext context) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    final snackbar = SnackBar(
+      content: const Text('Hello world'),
+      action: SnackBarAction(
+        label: 'Ok',
+        onPressed: () {},
+      ),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackbar);
+  }
+
+  void openDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        title: const Text('Are you sure?'),
+        content: const Text('Super long text for demostration purposes'),
+        actions: [
+          TextButton(
+              onPressed: () {
+                context.pop();
+              },
+              child: const Text('Cancel')),
+          FilledButton(
+              onPressed: () {
+                context.pop();
+              },
+              child: const Text('Accept')),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Snackbar & dialogs'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+              onPressed: () {
+                showAboutDialog(
+                    context: context,
+                    children: [const Text('Lorem impsum text')]);
+              },
+              child: const Text('Used licenses'),
+            ),
+            FilledButton.tonal(
+              onPressed: () => openDialog(context),
+              child: const Text('Show dialogs'),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text('Show snackbar'),
+        icon: const Icon(Icons.remove_red_eye_outlined),
+        onPressed: () {
+          showCustomSnackbar(context);
+        },
+      ),
+    );
   }
 }
